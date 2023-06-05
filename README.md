adc3883e3359fd03315e989bd249e6b3211d57b5
Ludmila <milaabl2405@gmail.com>
2023-06-05 18:03:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![](https://raw.githubusercontent.com/milaabl/milaabl/main/chat.svg)](https://www.linkedin.com/in/ludmila-a-dev/)
+<br/>
+<br/>
+<br/>
+<br/>
+![Jokes Card](https://readme-jokes.vercel.app/api)
---
+++ build-svg.js
@@ -0,0 +1,97 @@
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY
+
+let fs = require('fs')
+let got = require('got')
+let qty = require('js-quantities')
+let formatDistance = require('date-fns/formatDistance')
+
+let WEATHER_DOMAIN = 'http://dataservice.accuweather.com'
+
+const emojis = {
+  1: '☀️',
+  2: '☀️',
+  3: '🌤',
+  4: '🌤',
+  5: '🌤',
+  6: '🌥',
+  7: '☁️',
+  8: '☁️',
+  11: '🌫',
+  12: '🌧',
+  13: '🌦',
+  14: '🌦',
+  15: '⛈',
+  16: '⛈',
+  17: '🌦',
+  18: '🌧',
+  19: '🌨',
+  20: '🌨',
+  21: '🌨',
+  22: '❄️',
+  23: '❄️',
+  24: '🌧',
+  25: '🌧',
+  26: '🌧',
+  29: '🌧',
+  30: '🥵',
+  31: '🥶',
+  32: '💨',
+}
+
+// Cheap, janky way to have variable bubble width
+dayBubbleWidths = {
+  Monday: 235,
+  Tuesday: 235,
+  Wednesday: 260,
+  Thursday: 245,
+  Friday: 220,
+  Saturday: 245,
+  Sunday: 230,
+}
+
+// Time working at PlanetScale
+const today = new Date()
+const todayDay = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(
+  today
+)
+
+const psTime = formatDistance(new Date(2020, 12, 14), today, {
+  addSuffix: false,
+})
+
+// Today's weather
+const locationKey = '255'
+let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`
+console.log(url)
+got(url, { prefixUrl: WEATHER_DOMAIN })
+  .then((response) => {
+    console.log(response.body)
+    let json = JSON.parse(response.body)
+
+    const degF = Math.round(json.DailyForecasts[0].Temperature.Maximum.Value)
+    const degC = Math.round(qty(`${degF} tempF`).to('tempC').scalar)
+    const icon = json.DailyForecasts[0].Day.Icon
+
+    fs.readFile('template.svg', 'utf-8', (error, data) => {
+      if (error) {
+        return
+      }
+
+      data = data.replace('{degC}', degC)
+      data = data.replace('{weatherEmoji}', emojis[icon])
+      data = data.replace('{psTime}', psTime)
+      data = data.replace('{todayDay}', todayDay)
+      data = data.replace('{dayBubbleWidth}', dayBubbleWidths[todayDay])
+
+      data = fs.writeFile('chat.svg', data, (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })
+    })
+  })
+  .catch((err) => {
+    // TODO: something better
+    console.log(err)
+  })
